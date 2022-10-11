6fecd3465b32efdd2323a0875b0dc540ba200ca8
koenigm-seattleu <koenigm@seattleu.edu>
2022-10-10 17:39:12 -0700

    Added injection of Product Service, and call to GetAllData in Index.cshtml.cs

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
@@ -9,11 +14,27 @@ namespace ContosoCrafts.WebSite.Pages.Product
     public class IndexModel : PageModel
     {
         /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public IndexModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
         /// REST OnGet
         /// Return all the data
         /// </summary>
         public void OnGet()
         {
+            Products = ProductService.GetAllData();
         }
     }
 }
