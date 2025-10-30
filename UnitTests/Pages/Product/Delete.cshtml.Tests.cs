3591e324df42bc909785b5ff913573fd6d3cc86a
CJgracee <cjiang@seattleu.edu>
2025-10-29 18:15:59 -0700

    Add Delete feature in Delete.cshtml, Delete.cshtml.cs, and Delete.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Product;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace UnitTests.Pages.Product
+{
+    public class DeleteTests
+    {
+        private DeleteModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new DeleteModel(TestHelper.ProductService)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+                Url = TestHelper.UrlHelperFactory.GetUrlHelper(TestHelper.ActionContext),
+            };
+        }
+
+        [Test]
+        public void OnGet_Valid_Id_Should_Return_Product()
+        {
+            var product = TestHelper.ProductService.GetAllData().First();
+            var result = pageModel.OnGet(product.Id);
+            Assert.That(pageModel.Product, Is.Not.Null);
+            Assert.That(pageModel.Product.Id, Is.EqualTo(product.Id));
+        }
+
+        [Test]
+        public void OnGet_Invalid_Id_Should_Redirect_To_Index()
+        {
+            var result = pageModel.OnGet("invalid") as RedirectToPageResult;
+            Assert.That(result.PageName, Is.EqualTo("/Product/Index"));
+        }
+
+        [Test]
+        public void OnPost_Valid_Should_Delete_And_Redirect()
+        {
+            var product = TestHelper.ProductService.GetAllData().First();
+            pageModel.Product = product;
+            var result = pageModel.OnPost() as RedirectToPageResult;
+            Assert.That(result.PageName, Is.EqualTo("/Product/Index"));
+        }
+    }
+}
