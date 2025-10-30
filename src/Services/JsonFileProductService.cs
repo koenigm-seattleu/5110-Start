a563620c36f63e5fc6d0d05e560969b5ae446b5e
CJgracee <cjiang@seattleu.edu>
2025-10-29 17:47:15 -0700

    Fix minor syntax issue in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -150,10 +150,8 @@ namespace ContosoCrafts.WebSite.Services
                 Title = "Enter Title",
                 Description = "Enter Description",
                 Url = "Enter URL",
-                Image = "",
+                Image = ""
             };
-
-            // Get the current set, and append the new record to it because IEnumerable does not have Add
             var dataSet = GetAllData();
             dataSet = dataSet.Append(data);
 
