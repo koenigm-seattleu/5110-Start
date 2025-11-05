29c8a01262080a0af1291ce1d772b65610baed5e
CJgracee <cjiang@seattleu.edu>
2025-11-04 19:45:40 -0800

    Add missing unit tests to achieve full coverage for ReadModel in Read.cshtml.Tests.cs

--- UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -1,27 +1,60 @@
 using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages.Product;
 using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
 using Bunit;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UnitTests.Pages.Product
 {
     [TestFixture]
-    public class ReadTests : BunitTestContext
+    public class ReadTests
     {
+        private ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadModel(TestHelper.ProductService);
+        }
+
         [Test]
         public void OnGet_Valid_Should_Return_Product()
         {
             // Arrange
-            var webHostEnvironment = TestHelper.MockWebHostEnvironment.Object;
-            var productService = new JsonFileProductService(webHostEnvironment);
-            var pageModel = new ReadModel(productService);
+            var productId = TestHelper.ProductService.GetAllData().First().Id;
 
             // Act
-            pageModel.OnGet("jenlooper-light");
+            var result = pageModel.OnGet(productId) as PageResult;
 
             // Assert
+            Assert.That(result, Is.Not.Null);
             Assert.That(pageModel.Product, Is.Not.Null);
-            Assert.That(pageModel.Product.Id, Is.EqualTo("jenlooper-light"));
+            Assert.That(pageModel.Product.Id, Is.EqualTo(productId));
+        }
+
+        [Test]
+        public void OnGet_Null_Id_Should_Redirect_To_Index()
+        {
+            // Act
+            var result = pageModel.OnGet(null) as RedirectToPageResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.PageName, Is.EqualTo("/Product/Index"));
+        }
+
+        [Test]
+        public void OnGet_Invalid_Id_Should_Redirect_To_Index()
+        {
+            // Act
+            var result = pageModel.OnGet("invalid-id") as RedirectToPageResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.PageName, Is.EqualTo("/Product/Index"));
         }
     }
 }
