5bbdebb1c46e760486c5fdd10fbec0836ec35046
koenigm-seattleu <koenigm@seattleu.edu>
2025-11-05 16:06:30 -0800

    Fix Path to use Net8 instead of 6 in TestFixture.cs

--- UnitTests/TestFixture.cs
+++ UnitTests/TestFixture.cs
@@ -1,4 +1,4 @@
-using System.IO;
+﻿using System.IO;
 
 using NUnit.Framework;
 
@@ -26,7 +26,7 @@ namespace UnitTests
 
 
 
-            var DataWebPath = "../../../../src/bin/Debug/net6.0/wwwroot/data";
+            var DataWebPath = "../../../../src/bin/Debug/net8.0/wwwroot/data";
             var DataUTDirectory = "wwwroot";
             var DataUTPath = DataUTDirectory + "/data";
 
