c9623f318a45d72da18943196e2ae396060b5a32
CJgracee <cjiang@seattleu.edu>
2025-11-05 16:35:46 -0800

    Add unit test for CreateData() in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -77,6 +77,37 @@ namespace UnitTests.Pages.Product.AddRating
             Assert.That(dataNewList.Ratings.Last(), Is.EqualTo(5));
         }
         #endregion AddRating
+        
+        #region GetAllData
+        [Test]
+        public void GetAllData_Should_Return_Products()
+        {
+            // Act
+            var result = TestHelper.ProductService.GetAllData();
 
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.GreaterThan(0));
+        }
+        #endregion GetAllData
+        
+        #region CreateData
+        [Test]
+        public void CreateData_Valid_Should_Create_New_Product()
+        {
+            // Arrange
+            var oldCount = TestHelper.ProductService.GetAllData().Count();
+
+            // Act
+            var result = TestHelper.ProductService.CreateData();
+            var newCount = TestHelper.ProductService.GetAllData().Count();
+
+            // Assert
+            Assert.That(result, Is.Not.Null, "Created product should not be null");
+            Assert.That(result.Id, Is.Not.Empty, "Created product should have an ID");
+            Assert.That(newCount, Is.EqualTo(oldCount + 1), "Product count should increase by 1");
+        }
+        #endregion CreateData
+        
     }
 }
